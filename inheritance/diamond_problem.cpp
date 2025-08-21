40837f86ec705570923ccd98a2b191dad23da737
Sapana Dashoni <learnersapana100@gmail.com>
2025-08-21 23:18:32 +0530

    diamond biscuit problem

---
+++ inheritance/diamond_problem.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Base{
+    public:
+    void show() {
+        cout << "Hello from Base class!" << endl;
+    }
+};
+
+class Parent1 : virtual public Base {
+
+};
+
+class Parent2 : virtual public Base {
+
+};
+
+class Child  : public Parent1 , public Parent2{
+
+};
+
+int main() {
+
+    Child c1;
+    c1.show(); // deadly diamond of death problem
+
+
+    // there are 2 methods to resolve this prob
+    // 1. specify explicitly whose func you want to call
+    c1.Parent1::show();
+
+    //2. use virtal inheritance
+    // Without virtual → 2 copies of Base → ambiguity.
+
+    // With virtual → 1 shared Base → no ambiguity.
+
+
+
+    return 0;
+}
