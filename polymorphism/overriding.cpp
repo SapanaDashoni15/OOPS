50497aacdbc134d1f9cb0bf0e9f6391e62d1180c
Sapana Dashoni <learnersapana100@gmail.com>
2025-09-28 11:46:45 +0530

    added more info

--- polymorphism/overriding.cpp
+++ polymorphism/overriding.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Base{
     public:
-    void show() {
+    void virtual show() {
         cout << "Hello form base" << endl;
     }
 
@@ -20,10 +20,21 @@ class Child : public Base{
 
 int main() {
 
-    Child c1;
-    c1.show();
+    // Child c1;
+    // c1.show();
 
-    Base b1;
-    b1.show();
-    return 0;
+    // Base b1;
+    // b1.show();
+    // return 0;
+
+
+    // my base ptr has child obj, but i am still getting output as hello from base, this is happening because of realy binding
+
+    // this can be removed my making base class function as virtual
+    Base* bptr;
+    Child cptr;
+
+    bptr = &cptr;
+
+    bptr->show();
 }
